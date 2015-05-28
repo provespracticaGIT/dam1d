619f3bad0d7e32b3088adb264d02bdb93c553666
Xavi Roca <xrokag@gmail.com>
2015-05-28 22:47:15 +0200

    comit nuevo fichero repositorio GitHub

---
+++ MiClase.java
@@ -0,0 +1,14 @@
+
+public class MiClase {
+
+	public static void main(String[] args) {
+		System.out.println("Practica EGit");
+		System.out.println("Practica EGit 2");
+		System.out.println("Cambios conflicto rama master");
+		System.out.println("Cambios para fusionar");
+		System.out.println("Cambios para conflicto en testing2");
+		System.out.println("Cambio rebase master");
+				System.out.println("Cambio para rebase en testing3");
+	}
+
+}
